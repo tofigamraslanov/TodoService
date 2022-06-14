3e7c4a5293cf1216db8a74383fa160a57691d0e5
Tofiq Əmraslanov <tofiq.amraslanov.t@asoiu.edu.az>
2022-06-14 18:40:42 +0400

    🚀 Domain Layer Implemented

---
+++ Core/TodoService.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TodoService.Domain.Entities.Common
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        virtual public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ Core/TodoService.Domain/Entities/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using TodoService.Domain.Entities.Common;
+
+namespace TodoService.Domain.Entities
+{
+    public class TodoItem:BaseEntity
+    {
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
+    }
+}
